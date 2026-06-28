80f913635a5bb6b59c1134299884949e977cfbbf
AdityaNarayanMohanty9876 <160753473+AdityaNarayanMohanty9876@users.noreply.github.com>
2026-06-28 20:35:28 +0530

    Add files via upload

--- Python 1.py
+++ Python 1.py
@@ -3,6 +3,7 @@
 # print(23)
 # print(23+35)
 
+# finding out type
 # name = "Aditya Narayan Mohanty" 
 # age = 23
 # price = 25.99
@@ -11,6 +12,7 @@
 # print(type(age))
 # print(type(price))
 
+# finding out type none and boolean
 # age = 23
 # old = False
 # a = None
@@ -22,7 +24,44 @@
 # sum = a+b
 # print(sum)
 
-name = input("name: ")
+# Taking user or input keyboard
+# name = input("name: ")
+# age = int(input("age: "))
+# price = float(input("price: "))
+# print(name, age, price)
+
+# conditional statement
+# age = int(input("age: "))
+# if(age<18):
+#     print("Can't vote")
+# else:
+#     print("Can vote")    
+
+# lights = input("lights: ")
+# if(lights == "red"):
+#     print("Stop")
+# elif(lights == "yellow" ):
+#      print("Wait")   
+# elif(lights == "green"):
+#      print("go")
+# else:
+#      print("invalid statement")           
+
+# marks = int(input("marks: "))
+# if(marks>90):
+#     print("O GRADE")
+# elif(marks>80 and marks<90):
+#     print("A GRADE")
+# elif(marks>70 and marks<80):
+#     print("B GRADE")
+# else:
+#     print("fail")        
+
+# Ternary operator, conditional statement in one line
+# food = input("food: ")
+# eat = "yes" if food == "cake" else "no"
+# print (eat)
+ 
 age = int(input("age: "))
-price = float(input("price: "))
-print(name, age, price)
+vote = "Can vote" if age >= 18 else "cannot vote"
+print(vote)
