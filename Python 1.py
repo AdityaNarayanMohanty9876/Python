662f0e7c98d4a2ac563cfc1dbd73ba5863d9dc26
AdityaNarayanMohanty9876 <160753473+AdityaNarayanMohanty9876@users.noreply.github.com>
2026-06-28 19:22:06 +0530

    Add files via upload

---
+++ Python 1.py
@@ -0,0 +1,28 @@
+# print("Hello world")
+# print("Aditya mohanty, My age is 23")
+# print(23)
+# print(23+35)
+
+# name = "Aditya Narayan Mohanty" 
+# age = 23
+# price = 25.99
+# print(name,age,price)
+# print(type(name))
+# print(type(age))
+# print(type(price))
+
+# age = 23
+# old = False
+# a = None
+# print(type(old))
+# print(type(a))
+
+# a = 2 
+# b = 5
+# sum = a+b
+# print(sum)
+
+name = input("name: ")
+age = int(input("age: "))
+price = float(input("price: "))
+print(name, age, price)
