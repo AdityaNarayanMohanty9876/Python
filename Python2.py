ea9fda47d18d584b1d4843e20b68f7c556a2ceef
AdityaNarayanMohanty9876 <160753473+AdityaNarayanMohanty9876@users.noreply.github.com>
2026-06-30 19:21:36 +0530

    Add files via upload

---
+++ Python2.py
@@ -0,0 +1,62 @@
+# Arithmatic operators
+# a =int(input("a: "))
+# b =int(input("b: "))
+# sum=(a+b)
+# diff=(a-b)
+# mul=(a*b)
+# div=(a/b)
+# mod=(a%b)  #remainder
+# pow=(a**b) #power
+# print(sum, diff, mul, div, mod, pow)
+
+#Relational operator (output only in True or False)
+# a = int(input("a: "))
+# b = int(input("b: "))
+
+# rel1 = (a == b)
+# rel2 = (a != b)
+# rel3 = (a > b)
+# rel4 = (a < b)
+# rel5 = (a >= b)
+# rel6 = (a <= b)
+# print(rel1, rel2, rel3, rel4, rel5, rel6)
+
+# Assignment operator
+# a = int(input("a: "))
+# # a += 10  #(a = a+10 and a+=10 are same) 
+# # a -= 10
+# # a *=10
+# # a/=10
+# # a%=10
+# # a**=10
+# print(a)
+
+# Type conversion (automatically converts, we dont have to give any manual input)
+# a = 5
+# b = 2.5
+# sum = (a+b)
+# print(sum)
+
+# Type casting (manually how we used int to convert the value of a string into int, 
+# a=int("3")     #earlier it was giving error when we tried to convert str to float) 
+# b=2.5
+# # c=int(a)
+# print(type(a))
+# print (a+b)
+
+# Taking input
+# a = int(input("sides of the square: "))
+# area = a**2
+# print("Area: ", area )
+
+# a = float(input("a: "))
+# b = float(input("b: "))
+# average = (a*b)/2
+# print("Average: ", average)
+
+a= int(input("a: "))
+b= int(input("b: "))
+if(a>b):
+    print("True")
+else:
+    print("False")    
