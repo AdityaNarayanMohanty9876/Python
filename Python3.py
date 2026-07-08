f83a0a67221557e594b1e962ba3a5665b0dd2928
AdityaNarayanMohanty9876 <160753473+AdityaNarayanMohanty9876@users.noreply.github.com>
2026-07-08 06:26:35 +0530

    Add files via upload

---
+++ Python3.py
@@ -0,0 +1,33 @@
+# Strings
+# Str1 = "MY NAME IS ADITYA.\nI AM 23 YEARS OLD."
+# print(Str1)
+
+# str1 = "This is a string.\t WOW!"
+# print(str1)
+
+# # concatination a+b, "Hello" + "world"
+# str1 = "HELLO"
+# str2 = "world"
+# print(str1 + str2)
+
+# length of str
+# str1 = "My Name is Aditya."
+# print(len(str1))
+# print(str1[3])  #indexing (find the position)
+# print(str1[1:6]) #slicing (section lof string accessing)
+# print(str1[-4:-1]) #negative indexing
+
+
+# # String functions
+# str1 = "hi I am Aditya."
+# print(str1.endswith("tya.")) 
+# print(str1.capitalize())
+# print(str1.replace("i","o"))
+# print(str1.find("i"))
+# print(str1.count("i"))
+
+a = input("a: ")
+print(len(a))
+
+a = input("a:")
+print(a.count("S"))
